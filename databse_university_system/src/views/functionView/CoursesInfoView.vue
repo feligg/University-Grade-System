9ef1016cba6239ab3abb4b15e9d9882e7a88ebb5
feligg <felixjingga05@gmail.com>
2025-12-13 15:33:19 +0800

    Create CoursesInfoView.vue

---
+++ databse_university_system/src/views/functionView/CoursesInfoView.vue
@@ -0,0 +1,49 @@
+<template>
+<section class="page_404">
+  <div class="container">
+    <div class="row"> 
+    </div>
+  </div>
+</section>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+.page_404{ 
+    padding:20px 0; 
+    background:#fff; 
+    font-family: 'Arvo', serif; 
+    display: flex;               
+    justify-content: center;     
+    align-items: center;         
+    flex-direction: column; 
+    height: 100vh;  
+    text-align: center;
+
+}
+
+.four_zero_four_bg{
+  background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+  height: 400px;
+  background-position: center;
+}
+
+.four_zero_four_bg h1{
+  font-size:80px;
+}
+
+.link_404{
+  color: #fff!important;
+  padding: 10px 20px;
+  background: #667eea;
+  margin: 20px 0;
+  display: inline-block;
+}
+
+.contant_box_404{ 
+  margin-top: -50px;
+}
+</style>

e3c1101fd6306ef8fcbd6380840bebae90205b0f
feligg <felixjingga05@gmail.com>
2025-12-13 15:38:25 +0800

    Create NotFound.vue

---
+++ databse_university_system/src/views/functionView/NotFound.vue
@@ -0,0 +1,64 @@
+<template>
+<section class="page_404">
+  <div class="container">
+    <div class="row"> 
+      <div class="col-sm-12">
+        <div class="col-sm-10 col-sm-offset-1 text-center">
+          
+          <div class="four_zero_four_bg">
+            <h1 class="text-center">404</h1>
+          </div>
+
+          <div class="contant_box_404">
+            <h3 class="h2">Look like you're lost</h3>
+            <p>The page you are looking for is not available!</p>
+            
+            <router-link to="/" class="link_404">Go to Home</router-link>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+.page_404{ 
+    padding:20px 0; 
+    background:#fff; 
+    font-family: 'Arvo', serif; 
+    display: flex;               
+    justify-content: center;     
+    align-items: center;         
+    flex-direction: column; 
+    height: 100vh;  
+    text-align: center;
+
+}
+
+.four_zero_four_bg{
+  background-image: url(https://cdn.dribbble.com/users/285475/screenshots/2083086/dribbble_1.gif);
+  height: 400px;
+  background-position: center;
+}
+
+.four_zero_four_bg h1{
+  font-size:80px;
+}
+
+.link_404{
+  color: #fff!important;
+  padding: 10px 20px;
+  background: #667eea;
+  margin: 20px 0;
+  display: inline-block;
+}
+
+.contant_box_404{ 
+  margin-top: -50px;
+}
+</style>
